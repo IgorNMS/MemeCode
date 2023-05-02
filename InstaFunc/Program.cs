0bcfba90f3342a16b3371ef1e937fdedd46ed940
Igor <nascimentoigor49@gmail.com>
2023-05-02 16:07:56 -0300

    First commit

---
+++ InstaFunc/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace InstaFunc;
+
+public class Program
+{
+    public static int Calc(int n)
+    {
+        if (n == 0) return 2;
+        if (n == 1) return 1;
+        else return Calc(n - 1) + Calc(n - 2);
+    }
+
+    static void Main()
+    {
+        string s = "";
+
+        for (int i = 1; i < 6; i++)
+        {
+            s += Calc(i).ToString();
+        }
+        Console.WriteLine("www.multisoft.se/" + s);
+    }
+}
